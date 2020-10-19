f818e421c6a0845025827579f857179bf4fb313c
Yevgeny Sotnikov <yevgeny.sotnikov@gmail.com>
2020-10-19 10:24:28 +0300

    fix android + relayout first screen

--- src/Pokatun.Droid/Views/Main/MainContainerActivity.cs
+++ src/Pokatun.Droid/Views/Main/MainContainerActivity.cs
@@ -1,17 +1,44 @@
 ﻿
+using System;
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
 using Android.Views;
+using AndroidX.AppCompat.Widget;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Platforms.Android.Views;
 using Pokatun.Core.ViewModels.Main;
 
 namespace Pokatun.Droid.Views.Main
 {
+    [MvxActivityPresentation]
     [Activity(
         ScreenOrientation = ScreenOrientation.SensorPortrait,
         Theme = "@style/AppTheme",
         WindowSoftInputMode = SoftInput.AdjustResize | SoftInput.StateHidden)]
     public class MainContainerActivity : BaseActivity<MainContainerViewModel>
     {
+        private Toolbar _toolbar;
+
         protected override int ActivityLayoutId => Resource.Layout.activity_main_container;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            _toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(_toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayShowHomeEnabled(true);
+            SupportActionBar.SetDisplayShowTitleEnabled(false);
+        }
+
+        public override bool OnSupportNavigateUp()
+        {
+            OnBackPressed();
+
+            return true;
+        }
     }
 }
