710767e64fcdb006a619f7ceb0a53e3fd8c24e01
Yevgeny Sotnikov <yevgeny.sotnikov@gmail.com>
2020-11-05 21:34:19 +0200

    add check that token is saved

--- src/Pokatun.Core/AppStart.cs
+++ src/Pokatun.Core/AppStart.cs
@@ -2,19 +2,25 @@
 using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
 using Pokatun.Core.ViewModels.ChoiseUserRole;
+using Pokatun.Core.ViewModels.Menu;
+using Xamarin.Essentials.Interfaces;
 
 namespace Pokatun.Core
 {
     public class AppStart : MvxAppStart
     {
-        public AppStart(IMvxApplication app, IMvxNavigationService navigationService) : base(app, navigationService)
-        {
+        private readonly ISecureStorage _secureStorage;
 
+        public AppStart(IMvxApplication app, IMvxNavigationService navigationService, ISecureStorage secureStorage) : base(app, navigationService)
+        {
+            _secureStorage = secureStorage;
         }
 
         protected override async Task NavigateToFirstViewModel(object hint = null)
         {
-            await NavigationService.Navigate<ChoiseUserRoleViewModel>();
+            await (string.IsNullOrWhiteSpace(await _secureStorage.GetAsync(Constants.Keys.Token))
+                ? NavigationService.Navigate<ChoiseUserRoleViewModel>()
+                : NavigationService.Navigate<HotelMenuViewModel>());
         }
     }
 }
